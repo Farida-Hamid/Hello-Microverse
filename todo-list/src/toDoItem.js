3dd6b639a7d4ce547707a939adda68526c6c3725
Farida-Hamid <faridaaddala@gmail.com>
2022-07-14 11:52:16 +0300

    Add the toDoItem file

---
+++ todo-list/src/toDoItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+export class Todoitem extends Component {
+
+  getStyle = () => {
+    return {
+      padding: '5px',
+      textDecoration: this.props.todos.completed ? 'line-through' : 'none'
+    }
+  }
+
+  render() {
+    const { id, title }  = this.props.todos
+    return (
+      <div style={ this.getStyle() }>
+        <p> <input type='checkbox' onChange={this.props.markComplete.bind(this, id)}></input> { title }</p>
+      </div>
+    )
+  }
+}
+
+Todoitem.propTypes = {
+  todos: propTypes.object.isRequired
+}
+
+export default Todoitem
