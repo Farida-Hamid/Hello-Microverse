0f61b04e406df5c922875e6b28e8d53d7fc313dd
Farida-Hamid <faridaaddala@gmail.com>
2022-07-14 11:58:43 +0300

    Finalize check

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import TodoContainer from "./TodoContainer";
+import TodosList from './TodosList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <TodoContainer />
+    <TodosList />
   </React.StrictMode>
 );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-
-// import React from "react"
-// import ReactDOM from "react-dom"
-// //component file
-// import TodoContainer from "./components/TodoContainer"
-// ReactDOM.render(<TodoContainer />, document.getElementById("root"))
